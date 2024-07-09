30e26414e5c5af4d8e2183f182cd74d859f0b0fe
Emmanuel Asiimwe <90935867+asiimwemmanuel@users.noreply.github.com>
2024-07-09 15:14:53 +0300

    updating map iframe

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import './Map.css'; // Add CSS for styling
 
 const Map = () => {
   return (
     <div className="map-card">
-      <iframe
+    <iframe
         title="Google Map"
         className="map-iframe"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345094267!2d144.9556513156166!3d-37.817326979751554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf5779fd8a1cf4b6d!2sFlinders%20Street%20Station!5e0!3m2!1sen!2sau!4v1615332491376!5m2!1sen!2sau"
-        allowFullScreen=""
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.756543088486!2d32.56559517547489!3d0.31884229967802646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dbbd2ef435ddf%3A0x76e35f194cca43e6!2sBIIL%20House!5e0!3m2!1sen!2sug!4v1720526862067!5m2!1sen!2sug" 
+        allowfullscreen=""
         loading="lazy"
-      ></iframe>
+        referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
     </div>
   );
 };
